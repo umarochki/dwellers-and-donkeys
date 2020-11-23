d8ee6f605f7f3145b7aca4c228c74f39f86930c0
Kolonskaia Anastasiia <anagrimonia@gmail.com>
2020-11-23 12:50:42 +0300

    Added pan and zoom, drag'n'drop DOM-objects

--- game-module/src/App.js
+++ game-module/src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import GameMap from './GameMap';
+import GameMap from './components/GameMap';
 
 const WIDTH = window.innerWidth;
 const HEIGHT = window.innerHeight;
+const MAP_IMAGE_PATH = './map.png'
+const BG_COLOR = 0x333333;
 
 function App() {
   return (
     <div className="App">
-      <GameMap width={WIDTH} height={HEIGHT} />
+      <GameMap 
+        width={WIDTH} 
+        height={HEIGHT} 
+        mapImagePath={MAP_IMAGE_PATH}
+        backgroundColor={BG_COLOR}
+      />
     </div>
   );
 }
