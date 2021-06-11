c81329c6372f0e79bc9a7a93a13e914a9f3527fb
Aliya Safina <ktotamy@gmail.com>
2021-06-11 11:19:59 +0300

    add to SocketMessageType

--- nginx/tank/src/models/socket.ts
+++ nginx/tank/src/models/socket.ts
@@ -22,7 +22,21 @@ export enum SocketMessageType {
     Update = 'update',
     UpdateAndStart = 'update_and_start',
     UpdateAndSave = 'Update_and_save',
-    Delete = 'delete'
+    Delete = 'delete',
+
+    draw_pencil_started = 'draw_pencil_started',
+    draw_pencil_moved = 'draw_pencil_moved',
+    draw_pencil_stopped = 'draw_pencil_stopped',
+
+    draw_eraser_started = 'draw_eraser_started',
+    draw_eraser_moved = 'draw_eraser_moved',
+    draw_eraser_stopped = 'draw_eraser_stopped',
+
+    draw_polygon_started = 'draw_polygon_started',
+    draw_polygon_middle = 'draw_polygon_middle',
+    draw_polygon_stopped = 'draw_polygon_stopped',
+    draw_polygon_moved = 'draw_polygon_moved',
+    region_obstacle_add = 'region_obstacle_add'
 }
 
 export type SocketMessage =
@@ -38,6 +52,17 @@ export type SocketMessage =
     | UpdateAndSaveMessage
     | DeleteMessage
     | AddMessage
+    | draw_pencil_startedMessage
+    | draw_pencil_movedMessage
+    | draw_pencil_stoppedMessage
+    | draw_eraser_startedMessage
+    | draw_eraser_movedMessage
+    | draw_eraser_stoppedMessage
+    | draw_polygon_startedMessage
+    | draw_polygon_middleMessage
+    | draw_polygon_stoppedMessage
+    | draw_polygon_movedMessage
+    | region_obstacle_addMessage
 
 export interface SocketMessageBase {
     type: SocketMessageType
@@ -52,4 +77,49 @@ export interface DisconnectMessage extends SocketMessageBase {
 export interface ConnectMessage extends SocketMessageBase {
     type: SocketMessageType.Connect
     meta: ConnectedUser
+}
+
+export interface draw_pencil_startedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_pencil_started
+    meta: any
+}
+export interface draw_pencil_movedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_pencil_moved
+    meta: any
+}
+export interface draw_pencil_stoppedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_pencil_stopped
+    meta: any
+}
+export interface draw_eraser_startedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_eraser_started
+    meta: any
+}
+export interface draw_eraser_movedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_eraser_moved
+    meta: any
+}
+export interface draw_eraser_stoppedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_eraser_stopped
+    meta: any
+}
+export interface draw_polygon_startedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_polygon_started
+    meta: any
+}
+export interface draw_polygon_middleMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_polygon_middle
+    meta: any
+}
+export interface draw_polygon_stoppedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_polygon_stopped
+    meta: any
+}
+export interface draw_polygon_movedMessage extends SocketMessageBase {
+    type: SocketMessageType.draw_polygon_moved
+    meta: any
+}
+export interface region_obstacle_addMessage extends SocketMessageBase {
+    type: SocketMessageType.region_obstacle_add
+    meta: any
 }
